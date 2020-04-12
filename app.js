c52a0eafd80e987442f352be2d5e8ec24fbfdff3
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2020-04-12 15:59:56 +0530

    CORS Update

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ const app = express();
 
 */
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}));
 
 app.use(helmet());
 
