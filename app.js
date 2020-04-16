22e26f3c17738055e9f49874805697a79e4c6513
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2020-04-16 19:01:10 +0530

    Changing URL

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://savory-earth.surge.sh',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
